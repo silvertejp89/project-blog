b67d407f022106dbf3e3499980f418138929c227
silvertejp89 <kriss.stockhaus@gmail.com>
2022-04-19 15:39:29 +0200

    Staged

--- app.js
+++ app.js
@@ -12,11 +12,16 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-
+app.get("/", function (req, res) {
+  res.render("home", {
+    homeStartingContent
+  });
+})
 
 
 
