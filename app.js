afcddf7da4f9d17e01c8ffa70d2c624c8bd7b4da
silvertejp89 <kriss.stockhaus@gmail.com>
2022-05-02 10:19:10 +0200

    use lodash .lowerCase before compairing url and existing posts

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const { forEach } = require("lodash");
+const _ = require("lodash");
 
 //Content for Home, About and Contact
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -45,23 +45,22 @@ app.get("/compose", function(req, res) {
   res.render("compose");
 })
 
-//dynamic URL, checking if requested postname matches array
+//dynamic URL, checking if requested postname matches array, using lodash .lowerCase before compairing. 
 app.get("/posts/:postName", function(req, res) {
 
-  const requestedTitle = req.params.postName;
+  const requestedTitle = _.lowerCase(req.params.postName);
 
   posts.forEach(function(post){
-    const storedTitle = post.title;
+    const storedTitle = _.lowerCase(post.title);
     console.log("Stored title: ", storedTitle);
 
     if (storedTitle === requestedTitle) {
       console.log("Match found!")
     } else {
-      console.log("Match NOT found")
+      console.log("Match not found")
     }
   
   });
-
 
 });
 
